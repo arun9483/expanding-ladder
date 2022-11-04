daa978ba3dbd460beb17ab4267509bc90fea4c60
Arun Kumar Tripathi <arun9483@gmail.com>
2022-11-04 16:05:42 +0530

    [UPDATE] update total number of steps

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import Ladder from './components/Ladder';
 import './App.css';
 
+const totalSteps = 5; // update this number for number of steps
 function App() {
   return (
     <div className="App">
       <h2>Expanding Ladder</h2>
-      <Ladder steps={2} />
+      <Ladder steps={totalSteps} />
     </div>
   );
 }
